90a12196d6c45390cfa4305b015f0e06f15e4d0f
Lionel Goffaux <lionel.goffaux@unamur.be>
2026-05-29 14:59:00 +0200

    init commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+mod network_metric;
+mod relay_metric;
+
+#[derive(Parser)]
+enum Args {
+    RelayMetric,
+    BuildInference,
+    NetworkMetric,
+}
+
+fn main() {
+    let args = Args::parse();
+    match args {
+        Args::BuildInference => println!("build inference"),
+        Args::NetworkMetric => println!("network metric"),
+        Args::RelayMetric => println!("relay metric"),
+    }
+}
