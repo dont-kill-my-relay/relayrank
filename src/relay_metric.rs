6d1f46ffb74decf5ea04ae330eb945f18d28ed6f
Lionel Goffaux <lionel.goffaux@unamur.be>
2026-07-16 07:29:32 +0000

    improve relay metric perf

--- src/relay_metric.rs
+++ src/relay_metric.rs
@@ -1,12 +1,32 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use rayon::prelude::*;
 
 use crate::{
     parse_exclusion_list,
-    tor_status::{Consensus, Relay, RelayId},
+    tor_status::{self, Consensus},
 };
+
+struct Relay<'a> {
+    relay: &'a tor_status::Relay,
+    bwg: f64,
+    bwe: f64,
+}
+
+impl<'a> Relay<'a> {
+    fn is_exit(&self) -> bool {
+        self.relay.is_exit()
+    }
+
+    fn is_guard(&self) -> bool {
+        self.relay.is_guard()
+    }
+
+    fn reach(&self, other: &Relay) -> bool {
+        self.relay.reach(other.relay)
+    }
+}
 
 fn compute_bandwith_sums(consensus: &Consensus) -> (f64, f64) {
     consensus
@@ -31,93 +51,55 @@ fn compute_normalized_bandwidth(
     consensus: &Consensus,
     guard_bw_sum: f64,
     exit_bw_sum: f64,
-) -> (HashMap<&RelayId, f64>, HashMap<&RelayId, f64>) {
-    consensus.relays.iter().fold(
-        (HashMap::new(), HashMap::new()),
-        |(mut guards, mut exits), relay| {
-            if relay.is_guard() {
-                let bw = relay.bwg(&consensus.bandwidth_weights);
-                guards.insert(&relay.id, bw / guard_bw_sum);
-            }
-
-            if relay.is_exit() {
-                let bw = relay.bwe(&consensus.bandwidth_weights);
-                exits.insert(&relay.id, bw / exit_bw_sum);
-            }
-
-            (guards, exits)
-        },
-    )
+) -> Vec<Relay<'_>> {
+    consensus
+        .relays
+        .iter()
+        .map(|relay| {
+            let bwg = relay.bwg(&consensus.bandwidth_weights) / guard_bw_sum;
+            let bwe = relay.bwe(&consensus.bandwidth_weights) / exit_bw_sum;
+            Relay { relay, bwg, bwe }
+        })
+        .collect()
 }
 
-fn metric_guard(
-    guard: &Relay,
-    relays: &[Relay],
-    guard_bw: &HashMap<&RelayId, f64>,
-    exit_bw: &HashMap<&RelayId, f64>,
-) -> f64 {
-    let guard_value = guard_bw
-        .get(&guard.id)
-        .expect("sguard should be in guard_bw");
+fn metric_guard(guard: &Relay, relays: &[Relay]) -> f64 {
+    let guard_value = guard.bwg;
     let exits_value: f64 = relays
         .iter()
         .filter(|exit| exit.is_exit() && guard.reach(exit))
-        .map(|exit| exit_bw.get(&exit.id).expect("exit shoulb be in exit_bw"))
+        .map(|exit| exit.bwe)
         .sum();
     // dbg!((guard_value, exits_value));
     guard_value * exits_value
 }
 
-fn metric_exit(
-    exit: &Relay,
-    relays: &[Relay],
-    guard_bw: &HashMap<&RelayId, f64>,
-    exit_bw: &HashMap<&RelayId, f64>,
-) -> f64 {
-    let exit_value = exit_bw.get(&exit.id).expect("exit should be in exit_bw");
+fn metric_exit(exit: &Relay, relays: &[Relay]) -> f64 {
+    let exit_value = exit.bwe;
     let guards_value: f64 = relays
         .iter()
         .filter(|guard| guard.is_guard() && exit.reach(guard))
-        .map(|guard| {
-            guard_bw
-                .get(&guard.id)
-                .expect("guard should be in guard_bw")
-        })
+        .map(|guard| guard.bwg)
         .sum();
     // dbg!((exit_value, guards_value));
     exit_value * guards_value
 }
 
-fn metric_dual(
-    dual: &Relay,
-    relays: &[Relay],
-    guard_bw: &HashMap<&RelayId, f64>,
-    exit_bw: &HashMap<&RelayId, f64>,
-) -> f64 {
-    metric_guard(dual, relays, guard_bw, exit_bw) + metric_exit(dual, relays, guard_bw, exit_bw)
+fn metric_dual(dual: &Relay, relays: &[Relay]) -> f64 {
+    metric_guard(dual, relays) + metric_exit(dual, relays)
 }
 
 pub fn compute(consensus: &Consensus, exclusion_list: &Option<PathBuf>) -> Result<()> {
     let (guard_bw_sum, exit_bw_sum) = compute_bandwith_sums(consensus);
-    let (guard_bw, exit_bw) = compute_normalized_bandwidth(consensus, guard_bw_sum, exit_bw_sum);
+    let relays = compute_normalized_bandwidth(consensus, guard_bw_sum, exit_bw_sum);
 
-    let mut metric: Vec<_> = consensus
-        .relays
+    let mut metric: Vec<_> = relays
         .par_iter()
         .map(|relay| match (relay.is_guard(), relay.is_exit()) {
-            (true, true) => (
-                relay,
-                metric_dual(relay, &consensus.relays, &guard_bw, &exit_bw),
-            ),
-            (true, false) => (
-                relay,
-                metric_guard(relay, &consensus.relays, &guard_bw, &exit_bw),
-            ),
-            (false, true) => (
-                relay,
-                metric_exit(relay, &consensus.relays, &guard_bw, &exit_bw),
-            ),
-            (false, false) => (relay, 0.0),
+            (true, true) => (relay.relay, metric_dual(relay, &relays)),
+            (true, false) => (relay.relay, metric_guard(relay, &relays)),
+            (false, true) => (relay.relay, metric_exit(relay, &relays)),
+            (false, false) => (relay.relay, 0.0),
         })
         .collect();
 
